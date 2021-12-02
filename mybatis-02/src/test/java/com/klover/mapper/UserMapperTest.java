321b38ba4d5de2701845b67dae15e99abc721381
wjc <49420645@qq.com>
2021-12-02 21:42:33 +0800

    增加log4j

--- mybatis-02/src/test/java/com/klover/mapper/UserMapperTest.java
+++ mybatis-02/src/test/java/com/klover/mapper/UserMapperTest.java
@@ -3,20 +3,25 @@ package com.klover.mapper;
 import com.klover.entity.User;
 import com.klover.utils.MybatisUtils;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.log4j.Logger;
 import org.junit.Test;
 
 import java.util.List;
 
 public class UserMapperTest {
+    private static Logger logger = Logger.getLogger(UserMapperTest.class);
+
     @Test
     public void getUserList() {
+
         SqlSession sqlSession = MybatisUtils.getSqlSession();
 
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
         List<User> userList = mapper.getUserList();
 
         for (User user : userList) {
-            System.out.println(user);
+//            System.out.println(user);
+            logger.info(user);
         }
         sqlSession.close();
     }
